c4af111662575267d59948bdff781be2ba67af72
Pataki András <anpataki@gmail.com>
2020-08-12 20:10:16 +0200

    Open Weather Map token removed

--- wget_fetchers/forecast.bat
+++ wget_fetchers/forecast.bat
@@ -1,20 +1,23 @@
 @echo off
-rem Paks:
-D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=46.622047&lon=18.855918&units=metric&appid=47bb5d4c8c13479e5fddf7778605a7b9"
-rem French Alps:
-rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=47&lon=6&units=metric&appid=47bb5d4c8c13479e5fddf7778605a7b9"
-rem NYC:
-rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=40.76&lon=-73.98&units=metric&appid=47bb5d4c8c13479e5fddf7778605a7b9"
-rem Dublin:
-rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=53.34&lon=-6.26&units=metric&appid=47bb5d4c8c13479e5fddf7778605a7b9"
-rem Botswana:
-rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=-24.63&lon=25.90&units=metric&appid=47bb5d4c8c13479e5fddf7778605a7b9"
 
 setlocal
+set /p openweathermaptoken=<open-weather-map-token.txt
+
 set srcfile=d:\User\grid_desk\fetched_data\weather.txt
 set destfile=d:\User\grid_desk\fetched_data\weather-actual.txt
 set logfile=d:\User\grid_desk\fetched_data\weather.log
 set minbytesize=10000
+
+rem Paks:
+D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=46.622047&lon=18.855918&units=metric&appid=%openweathermaptoken%"
+rem French Alps:
+rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=47&lon=6&units=metric&appid=%openweathermaptoken%"
+rem NYC:
+rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=40.76&lon=-73.98&units=metric&appid=%openweathermaptoken%"
+rem Dublin:
+rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=53.34&lon=-6.26&units=metric&appid=%openweathermaptoken%"
+rem Botswana:
+rem D:\User\wget.exe --no-check-certificate -O d:\User\grid_desk\fetched_data\weather.txt "https://api.openweathermap.org/data/2.5/forecast?lat=-24.63&lon=25.90&units=metric&appid=%openweathermaptoken%"
 
 time /t>>%logfile%
 
